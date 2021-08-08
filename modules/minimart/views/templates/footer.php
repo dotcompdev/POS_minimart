bcd8e5313a6353294ada5ff50a0665540257a8a1
argaaryadipa <73064867+argaaryadipa@users.noreply.github.com>
2021-08-08 20:15:53 +0800

    Delete modules/minimart directory

--- modules/minimart/views/templates/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">Dotcomp</a>.</strong>
-    Make with proud.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Versi</b> 1.0.0
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
