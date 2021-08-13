b151d00bb256442caac14b8fcb1aa0835de97543
Raymond-15 <raymondputra15@gmail.com>
2021-08-13 11:01:10 +0800

    fix position form registration

--- application/views/supervisor/dashboardV.php
+++ application/views/supervisor/dashboardV.php
@@ -104,10 +104,3 @@
         </div>
     </section>
 </div>
-<footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="#">Dotcomp</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0
-    </div>
-</footer>
