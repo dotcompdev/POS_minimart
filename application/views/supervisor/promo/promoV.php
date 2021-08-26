97446e01e1ba79fc61275868ea3f24a2ed687b4a
argaaryadipa <argaaryadipa10@gmail.com>
2021-08-26 13:04:39 +0800

    add composer

--- application/views/supervisor/promo/promoV.php
+++ application/views/supervisor/promo/promoV.php
@@ -45,88 +45,100 @@
               <!-- /.card-header -->
               <div class="card-body">
                 <!-- TABEL -->
-            <div class="row">
-                <div class="col-lg">
-                    <div class="card-body table-responsive p-0" style="height: 250px;">
-                        <table class="table table-head-fixed text-nowrap">
-                            <thead>
-                                <tr>
-                                    <th width="100px">Aksi</th>
-                                    <th style="text-align: center;" width="100px">ID Promo</th>
-                                    <th width="100px">Nama Promo</th>
-                                    <th style="text-align: center;" width="150px">Harga Promo</th>
-                                    <th style="text-align: center;" width="100px">ID Bundle</th>
-                                    <th style="text-align: center;" width="100px">ID Jadwal</th>
-                                </tr>
-                            </thead>
+                <div class="row">
+                    <div class="col">
+                        <div class="card-body table-responsive p-0" style="height: 300px;">
+                            <table class="table table-head-fixed text-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th style="text-align: center;" width="100px">Aksi</th>
+                                        <th style="text-align: center;" width="100px">ID Promo</th>
+                                        <th width="100px">Nama Promo</th>
+                                        <th style="text-align: center;" width="150px">Harga Promo</th>
+                                        <th style="text-align: center;" width="100px">ID Bundle</th>
+                                        <th style="text-align: center;" width="100px">ID Jadwal</th>
+                                        <th style="text-align: center;" width="100px">Keterangan</th>
+                                    </tr>
+                                </thead>
 
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
-                                        <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
-                                    </td>
-                                    <td align="center">001</td>
-                                    <td>Promo Sayap Kebhinekaan</td>
-                                    <td align="center">20000</td>
-                                    <td align="center">B001</td>
-                                    <td align="center">J001</td>
-                                </tr>
-                                <tr>    
-                                    <td>
-                                        <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
-                                        <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
-                                    </td>
-                                    <td align="center">001</td>
-                                    <td>Promo Sayap Kebhinekaan</td>
-                                    <td align="center">20000</td>
-                                    <td align="center">B001</td>
-                                    <td align="center">J001</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
-                                        <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
-                                    </td>
-                                    <td align="center">001</td>
-                                    <td>Promo Sayap Kebhinekaan</td>
-                                    <td align="center">20000</td>
-                                    <td align="center">B001</td>
-                                    <td align="center">J001</td>
-                                </tr>
-                                <tr>    
-                                    <td>
-                                        <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
-                                        <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
-                                    </td>
-                                    <td align="center">001</td>
-                                    <td>Promo Sayap Kebhinekaan</td>
-                                    <td align="center">20000</td>
-                                    <td align="center">B001</td>
-                                    <td align="center">J001</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
-                                        <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
-                                    </td>
-                                    <td align="center">001</td>
-                                    <td>Promo Sayap Kebhinekaan</td>
-                                    <td align="center">20000</td>
-                                    <td align="center">B001</td>
-                                    <td align="center">J001</td>
-                                </tr>
-                                
-                            </tbody>
-                        </table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <a href="" type="button" class="btn btn-info far fa-eye btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
+                                        </td>
+                                        <td align="center">001</td>
+                                        <td>Promo Sayap Kebhinekaan</td>
+                                        <td align="center">20000</td>
+                                        <td align="center">B001</td>
+                                        <td align="center">J001</td>
+                                        <td>Available</td>
+                                    </tr>
+                                    <tr>    
+                                        <td>
+                                            <a href="" type="button" class="btn btn-info far fa-eye btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
+                                        </td>
+                                        <td align="center">001</td>
+                                        <td>Promo Sayap Kebhinekaan</td>
+                                        <td align="center">20000</td>
+                                        <td align="center">B001</td>
+                                        <td align="center">J001</td>
+                                        <td>Available</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="" type="button" class="btn btn-info far fa-eye btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
+                                        </td>
+                                        <td align="center">001</td>
+                                        <td>Promo Sayap Kebhinekaan</td>
+                                        <td align="center">20000</td>
+                                        <td align="center">B001</td>
+                                        <td align="center">J001</td>
+                                        <td>Available</td>
+                                    </tr>
+                                    <tr>    
+                                        <td>
+                                            <a href="" type="button" class="btn btn-info far fa-eye btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
+                                        </td>
+                                        <td align="center">001</td>
+                                        <td>Promo Sayap Kebhinekaan</td>
+                                        <td align="center">20000</td>
+                                        <td align="center">B001</td>
+                                        <td align="center">J001</td>
+                                        <td>Available</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="" type="button" class="btn btn-info far fa-eye btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-warning far fa-edit btn-sm"></a>
+                                            <a href="" type="button" class="btn btn-danger far fa-trash-alt btn-sm"></a>
+                                        </td>
+                                        <td align="center">001</td>
+                                        <td>Promo Sayap Kebhinekaan</td>
+                                        <td align="center">20000</td>
+                                        <td align="center">B001</td>
+                                        <td align="center">J001</td>
+                                        <td>Available</td>
+                                    </tr>
+                                    
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
+                <!-- END TABEL -->
             </div>
-            <!-- END TABEL -->
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        
         
     </div>
     <footer class="main-footer">
